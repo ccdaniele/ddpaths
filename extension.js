24085101ff3c9bce30e592690773f80480204241
daniel calderon <daniel.calderon@datadoghq.com>
2024-12-04 11:32:31 -0500

    logs over working

--- extension.js
+++ extension.js
@@ -3,13 +3,41 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+
+let decorationType;
+
 function activate(context) {
+
+    decorationType = vscode.window.createTextEditorDecorationType({
+        textDecoration: 'underline',
+        color: 'blue',
+        cursor: 'pointer'
+    });
+
+    // Register event listeners
+    // let openListener = vscode.workspace.onDidOpenTextDocument((document) => {
+    //     if (document.fileName.endsWith('g')) {
+    //         decorateEditor();
+    //     }
+    // });
+
+    // let changeListener = vscode.workspace.onDidChangeTextDocument((event) => {
+    //     const editor = vscode.window.activeTextEditor;
+    //     if (editor && editor.document.fileName.endsWith('g')) {
+    //         decorateEditor();
+    //     }
+    // });
 
     console.log('Extension activated!');
 
     // Check currently open documents
     vscode.workspace.textDocuments.forEach((document) => {
         console.log('Already open:', document.fileName);
+		if (document.fileName.endsWith('g')) {
+            console.log('true')
+			decorateEditor();
+        }
     });
 
     // Register the command
@@ -20,6 +48,38 @@ function activate(context) {
 
     context.subscriptions.push(disposable);
 }
+
+
+function decorateEditor() {
+
+	console.log ('runnning decorate editor')
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
+
+    const text = editor.document.getText();
+    const decorations = [];
+    const timestampRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC/;
+
+    text.split('\n').forEach((line, lineIndex) => {
+        const match = line.match(/\(.*?\)/);
+        if (timestampRegex.test(line) && match) {
+            const clickableSection = match[0].split(' ')[0].slice(1); // Extract content
+            const start = line.indexOf(match[0]);
+            const range = new vscode.Range(
+                new vscode.Position(lineIndex, start),
+                new vscode.Position(lineIndex, start + match[0].length)
+            );
+            decorations.push({
+                range,
+                hoverMessage: `Click to view ${clickableSection}`,
+            });
+        }
+    });
+
+    // Apply decorations
+    editor.setDecorations(decorationType, decorations);
+}
+
 
 // This method is called when your extension is deactivated
 function deactivate() {}
