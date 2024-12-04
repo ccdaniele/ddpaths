764d0c5b2e3ede99bd302f68750b9355ab06062e
daniel calderon <daniel.calderon@datadoghq.com>
2024-12-04 11:08:49 -0500

    2

--- extension.js
+++ extension.js
@@ -1,36 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ddpaths" is now active!');
+    console.log('Extension activated!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ddpaths.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+    // Check currently open documents
+    vscode.workspace.textDocuments.forEach((document) => {
+        console.log('Already open:', document.fileName);
+    });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ddpaths!');
-	});
+    // Register the command
+    const disposable = vscode.commands.registerCommand('ddpaths.helloWorld', function () {
+        console.log('Starting disposable');
+        vscode.window.showInformationMessage('Hello World from ddpaths!');
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
